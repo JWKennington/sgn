3898de3e472f99996c87aa438e05db63acbf5af4
patrick.godwin <patrick.godwin@ligo.org>
2024-05-24 10:36:25 -0700

    upstream EOS logic to SinkElement, add mark_eos method

--- src/sgn/sinks/__init__.py
+++ src/sgn/sinks/__init__.py
@@ -9,22 +9,11 @@ class FakeSink(SinkElement):
     A fake sink element
     """
 
-    def __post_init__(self):
-        super().__post_init__()
-        self.at_eos = {p: False for p in self.sink_pads}
-
     def pull(self, pad: SinkPad, bufs: list[Buffer]) -> None:
         """
         getting the buffer on the pad just modifies the name to show this final
         graph point and the prints it to prove it all works.
         """
-        self.at_eos[pad] = bufs[-1].EOS
+        if bufs[-1].EOS:
+            self.mark_eos(pad)
         print("buffer flow: ", "%s -> '%s'" % (bufs[-1].metadata["name"], pad.name))
-
-    @property
-    def EOS(self) -> bool:
-        """
-        If buffers on any sink pads are End of Stream (EOS), then mark this
-        whole element as EOS
-        """
-        return any(self.at_eos.values())
