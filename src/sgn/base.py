b7d95ce021fbd1669e0c925583c36b37e18a8969
patrick.godwin <patrick.godwin@ligo.org>
2024-06-24 10:33:21 -0700

    change class hierarchy for base classes

--- src/sgn/base.py
+++ src/sgn/base.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import uuid
 from dataclasses import dataclass, field
-from typing import Callable, ClassVar, Optional
+from typing import Callable, Optional, Union
 
 
 @dataclass
@@ -29,8 +29,20 @@ class Frame:
         self.metadata["__graph__"] = ""
 
 
+class _PostInitBase:
+    """
+    Used to resolve issues with the builtin object class with __post_init__
+    and using multiple inheritance. See https://stackoverflow.com/a/59987363/23231262.
+    """
+
+    def __post_init__(self):
+        # just intercept the __post_init__ calls so they
+        # aren't relayed to `object`
+        pass
+
+
 @dataclass
-class Base:
+class UniqueID(_PostInitBase):
     """
     A generic class from which all classes that participate in an execution
     graph should be derived.  It enforces a unique name and hashes based on that
@@ -44,27 +56,29 @@ class Base:
 
     """
 
-    name: Optional[str] = None
+    name: str = ""
+    _id: str = field(init=False)
 
     def __post_init__(self):
+        super().__post_init__()
         # give every element a truly unique identifier
-        self.__id = uuid.uuid4().hex
-        if self.name is None:
-            self.name = self.__id
+        self._id = uuid.uuid4().hex
+        if not self.name:
+            self.name = self._id
 
     # Note: we need the Base class to be hashable, so that it can be
     # used as a key in a dictionary, but mutable dataclasses are not
     # hashable by default, so we have to define our own hash function
     # here.
     def __hash__(self) -> int:
-        return hash(self.__id)
+        return hash(self._id)
 
     def __eq__(self, other) -> bool:
         return hash(self) == hash(other)
 
 
 @dataclass(eq=False, repr=False)
-class Pad(Base):
+class PadLike:
     """
     Pads are 1:1 with graph nodes but source and sink pads must be grouped into
     elements in order to exchange data from sink->source.  source->sink
@@ -80,23 +94,22 @@ class Pad(Base):
 
     Parameters
     ----------
-    element : Element, optional
-        The Element instance associated with this pad, default None
-    call : Callable, optional
-        The function that will be called during graph execution for this pad,
-        default None
+    element : Element
+        The Element instance associated with this pad
+    call : Callable
+        The function that will be called during graph execution for this pad
 
     """
 
-    element: Optional[Element] = None
-    call: Optional[Callable] = None
+    element: Element
+    call: Callable
 
     async def __call__(self) -> None:
         raise NotImplementedError
 
 
 @dataclass(eq=False, repr=False)
-class SourcePad(Pad):
+class _SourcePadLike(PadLike):
     """
     A pad that provides a data Frame when called.
 
@@ -109,18 +122,9 @@ class SourcePad(Pad):
 
     output: Optional[Frame] = None
 
-    async def __call__(self) -> None:
-        """
-        When called, a source pad receives a Frame from the element that
-        the pad belongs to.
-
-        """
-        self.output = self.call(pad=self)
-        self.output.metadata["__graph__"] += "-> %s " % self.name
-
 
 @dataclass(eq=False, repr=False)
-class SinkPad(Pad):
+class _SinkPadLike(PadLike):
     """
     A pad that receives a data Frame when called.  When linked, it returns
     a dictionary suitable for building a graph in graphlib.
@@ -137,6 +141,45 @@ class SinkPad(Pad):
     other: Optional[SourcePad] = None
     input: Optional[Frame] = None
 
+
+@dataclass(eq=False, repr=False)
+class SourcePad(UniqueID, _SourcePadLike):
+    """
+    A pad that provides a data Frame when called.
+
+    Parameters
+    ----------
+    output : Frame, optional
+        This attribute is set to be the output Frame when the pad is called.
+
+    """
+
+    async def __call__(self) -> None:
+        """
+        When called, a source pad receives a Frame from the element that
+        the pad belongs to.
+
+        """
+        self.output = self.call(pad=self)
+        assert isinstance(self.output, Frame)
+        self.output.metadata["__graph__"] += "-> %s " % self.name
+
+
+@dataclass(eq=False, repr=False)
+class SinkPad(UniqueID, _SinkPadLike):
+    """
+    A pad that receives a data Frame when called.  When linked, it returns
+    a dictionary suitable for building a graph in graphlib.
+
+    Parameters
+    ----------
+    other: SourcePad, optional
+        This holds the source pad that is linked to this sink pad, default None
+    input: Frame, optional
+        This holds the Frame provided by the linked source pad. Generally it
+        gets set when this SinkPad is called, default None
+    """
+
     def link(self, other: SourcePad) -> dict[SinkPad, set[SourcePad]]:
         """
         Only sink pads can be linked. A sink pad can be linked to only one
@@ -144,7 +187,7 @@ class SinkPad(Pad):
         Returns a dictionary of dependencies suitable for adding to a graphlib
         graph.
         """
-        assert isinstance(other, SourcePad), "other is not an instanace of SourcePad"
+        assert isinstance(other, SourcePad), "other is not an instance of SourcePad"
         self.other = other
         return {self: set((other,))}
 
@@ -158,13 +201,15 @@ class SinkPad(Pad):
         class.
 
         """
+        assert isinstance(self.other, SourcePad), "Sink pad has not been linked"
         self.input = self.other.output
+        assert isinstance(self.input, Frame)
         self.input.metadata["__graph__"] += "-> %s " % self.name
         self.call(self, self.input)
 
 
 @dataclass(repr=False)
-class Element(Base):
+class ElementLike(_PostInitBase):
     """
     A basic container to hold source and sink pads. The assmption is that this
     will be a base class for code that actually does something. It should never
@@ -186,6 +231,7 @@ class Element(Base):
     graph: dict[SourcePad, set[SinkPad]] = field(init=False)
 
     def __post_init__(self):
+        super().__post_init__()
         self.graph = {}
 
     @property
@@ -198,11 +244,11 @@ class Element(Base):
 
     @property
     def pad_list(self) -> list[Pad]:
-        return (self.source_pads or []) + (self.sink_pads or [])
+        return self.source_pads + self.sink_pads
 
 
 @dataclass(repr=False)
-class SourceElement(Element):
+class SourceElement(UniqueID, ElementLike):
     """
     Initialize with a list of source pads. Every source pad is added to the
     graph with no dependencies.
@@ -218,11 +264,11 @@ class SourceElement(Element):
     source_pad_names: list[str] = field(default_factory=list)
 
     def __post_init__(self):
+        super().__post_init__()
         self.source_pads = [
             SourcePad(name="%s:src:%s" % (self.name, n), element=self, call=self.new)
             for n in self.source_pad_names
         ]
-        super().__post_init__()
         assert self.source_pads and not self.sink_pads
         self.graph.update({s: set() for s in self.source_pads})
 
@@ -234,7 +280,7 @@ class SourceElement(Element):
 
 
 @dataclass(repr=False)
-class TransformElement(Element):
+class TransformElement(UniqueID, ElementLike):
     """
     Both "source_pads" and "sink_pads" must exist.  All sink pads depend on all
     source pads in a transform element. If you don't want that to be true,
@@ -256,6 +302,7 @@ class TransformElement(Element):
     sink_pad_names: list[str] = field(default_factory=list)
 
     def __post_init__(self):
+        super().__post_init__()
         self.source_pads = [
             SourcePad(
                 name="%s:src:%s" % (self.name, n),
@@ -268,7 +315,6 @@ class TransformElement(Element):
             SinkPad(name="%s:sink:%s" % (self.name, n), element=self, call=self.pull)
             for n in self.sink_pad_names
         ]
-        super().__post_init__()
         assert self.source_pads and self.sink_pads
         self.graph.update({s: set(self.sink_pads) for s in self.source_pads})
 
@@ -280,7 +326,7 @@ class TransformElement(Element):
 
 
 @dataclass
-class SinkElement(Element):
+class SinkElement(UniqueID, ElementLike):
     """
     "sink_pads" must exist but not "source_pads"
 
@@ -295,12 +341,12 @@ class SinkElement(Element):
     sink_pad_names: list[str] = field(default_factory=list)
 
     def __post_init__(self):
+        super().__post_init__()
         self.sink_pads = [
             SinkPad(name="%s:sink:%s" % (self.name, n), element=self, call=self.pull)
             for n in self.sink_pad_names
         ]
         self._at_eos = {p: False for p in self.sink_pads}
-        super().__post_init__()
         assert self.sink_pads and not self.source_pads
 
     @property
@@ -319,3 +365,7 @@ class SinkElement(Element):
 
     def pull(self, pad: SinkPad, frame: Frame) -> None:
         raise NotImplementedError
+
+
+Element = Union[TransformElement, SinkElement, SourceElement]
+Pad = Union[SinkPad, SourcePad]
