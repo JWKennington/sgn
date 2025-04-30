1cd0ad1afddf4bd72defaf515c8c409ee5158f5b
patrick.godwin <patrick.godwin@ligo.org>
2025-04-30 15:33:32 -0700

    remove commented out code

--- src/sgn/visualize.py
+++ src/sgn/visualize.py
@@ -25,28 +25,6 @@ def _color(node):
         return "MediumAquaMarine"
 
 
-# def _element_struct_record(element):
-#     """render element as "record" structure"""
-#     columns = []
-#     ss = []
-#     for snk in element.sink_pads:
-#         snk_id = _id(snk.name)
-#         snk_name = element.rsnks[snk]
-#         ss.append(f"<{snk_id}> {snk_name}")
-#     if ss:
-#         columns.append("{" + "|".join(ss) + "}")
-#     columns.append("")
-#     ss = []
-#     for src in element.source_pads:
-#         src_id = _id(src.name)
-#         src_name = element.rsrcs[src]
-#         ss.append(f"<{src_id}> {src_name}")
-#     if ss:
-#         columns.append("{" + "|".join(ss) + "}")
-#     struct = element.name + "|" + "{" + "|".join(columns) + "}"
-#     return struct
-
-
 def _element_struct_plaintext(element):
     """render element as html structure"""
     etype = element.__class__.__name__
@@ -129,7 +107,6 @@ def visualize(pipeline, label=None, path=None):
             "ranksep": "2",
         },
         node_attr={
-            # "shape": "record",
             "shape": "plaintext",
             "fontname": "times mono",
         },
