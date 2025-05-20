ee3f1becf1e66c974c0a1fa52ba08f34e9a5a102
Jameson Graef Rollins <jrollins@finestructure.net>
2025-05-19 18:04:49 -0700

    update default version to be something parsable

--- src/sgn/__init__.py
+++ src/sgn/__init__.py
@@ -6,7 +6,7 @@ import flattening and version handling
 try:
     from ._version import version as __version__
 except ImportError:
-    __version__ = "?.?.?"
+    __version__ = "0.0.0"
 
 # Import flattening
 from sgn.base import SourcePad, SinkPad, TransformElement, SourceElement, SinkElement
